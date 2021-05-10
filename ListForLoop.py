5c7fb083991beb71cbda18eff7fda9db4c4611a4
VipinAmbat <83779855+VipinAmbat@users.noreply.github.com>
2021-05-10 14:53:33 +0530

    Add files via upload
    Exercise for loop- Second assignment to collect postive no

---
+++ ListForLoop.py
@@ -0,0 +1,7 @@
+#Python program to continue with list having negative value
+
+mylist_num=[10,20,30,40,-10,-20,-30,-40,-50,70,80,90]#List having positive and negative num
+
+for num in mylist_num:#For loop
+    if num>=0:
+        print(num)
