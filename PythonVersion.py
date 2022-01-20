7050b39cf6ae817087e031711ed654d334fda9d8
Ambat Vipin <83779855+VipinAmbat@users.noreply.github.com>
2022-01-20 20:37:04 +0530

    Add files via upload
    Python code to find version of Python

---
+++ PythonVersion.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+# program to find version of Python
+import sys
+print("Python version is")
+print(sys.version)
+print("Version info.")
+print (sys.version_info)
+
+
+# In[ ]:
+
+
+
+
