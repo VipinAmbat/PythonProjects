2cd2badb1b70b6b0c4f347e854d9157c004cf37c
Ambat Vipin <ambat.vipu@gmail.com>
2022-04-24 13:32:57 +0530

    Variable demo
    A simple program to display how variable works, type of variable

---
+++ VaraiableDemo.py
@@ -0,0 +1,31 @@
+#  Program to exchange two number with a new variable
+
+print(" Assigning variable a with 7")
+print("Assigning variable b with value 10")
+
+a = 7  # a is assigned with 7
+b = 10  # b is assigned with 10
+
+c = a  # c is a new variable
+
+a = b
+
+b = c
+
+print("Now the Value of a is ", a)
+print("Now the Value of a is ", b)
+
+#  Program to find data type of the variable
+
+print("Data type of variable a is ")
+print(type(a))
+
+b = 90.89
+
+print("Data type of variable b is ")
+print(type(b))
+
+c = " My life is good"
+
+print(type(c))
+
