1fbc8af49a189efa4244c034d29727b63e9d1bcb
Ambat Vipin <ambat.vipu@gmail.com>
2022-04-24 16:39:28 +0530

    Even odd logic
    Program to find if number is even or odd in Python

---
+++ EvenOdd.py
@@ -0,0 +1,19 @@
+# Program to take an inout from user and verify if the number is even or odd
+
+# Line to take input
+print("Enter a number ")
+x = input()
+
+# Cast to an integer
+
+x = int(x)
+
+if x%2 == 0:
+
+    print("Entered number is even",x)
+
+else:
+
+    print("Entered number is odd" ,x)
+
+
